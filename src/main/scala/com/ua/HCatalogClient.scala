d09003e6447f039a03d1b732667b836b1e63a723
Maria Rabyk <mariia.rabyk@gmail.com>
2018-08-30 16:34:48 +0300

    Added methods for getting max batchId

--- src/main/scala/com/ua/HCatalogClient.scala
+++ src/main/scala/com/ua/HCatalogClient.scala
@@ -6,18 +6,43 @@ import org.apache.hive.hcatalog.api.HCatClient
 
 import scala.collection.JavaConverters._
 
-class HCatalogClient {
+class HCatalogClient(file: String) {
 
-  private def createHCatClient(file: String): HCatClient = {
-    val config = new Configuration()
-    config.addResource(new Path(file))
-    HCatClient.create(config)
+  val config = new Configuration()
+  config.addResource(new Path(file))
+  val hCatClient = HCatClient.create(config)
+
+  def getMaxBatchId(dbName: String, tableName: String, colunmName: String): Long = {
+
+    /*val spec = hCatClient.getPartitionSpecs(dbName, tableName, 10).getPartitionIterator
+    while(spec.hasNext) {
+      println("part value: " + spec.next().getPartitionKeyValMap.asScala.toMap.toString())
+    }
+*/
+    val partitionValues = getPartitionValues(dbName, tableName)
+    val columns = getPartitionColumns(dbName, tableName)
+
+    val columnsData = partitionValues
+      .flatMap(value => value.zip(columns))
+      .filter { case (value, columnName) => columnName == colunmName }
+      .map { case (value, columnName) => value.toLong }
+
+    println("List size: " + columnsData.length)
+    println("Max batch id: " + columnsData.max)
+
+    columnsData.max
   }
 
-  def getPartitionValues(dbName: String, tableName: String, file: String): List[String] = {
-    val hCatClient = createHCatClient(file)
-    val partitons = hCatClient.getPartitions(dbName, tableName).asScala
-    partitons.map(p => p.getValues.asScala.toList).toList.flatten
+
+  private def getPartitionColumns(dbName: String, tableName: String): List[String] = {
+    val partCols = hCatClient.getTable(dbName, tableName).getPartCols.asScala
+    partCols.map(col => col.getName).toList
+  }
+
+  private def getPartitionValues(dbName: String, tableName: String): List[List[String]] = {
+    val partitions = hCatClient.getPartitions(dbName, tableName).asScala
+    val values = partitions.map(_.getValues.asScala.toList).toList
+    values
   }
 
 }
