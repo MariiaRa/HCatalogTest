5558ce4486aec724c0df028698c7fd4749846a02
Maria Rabyk <mariia.rabyk@gmail.com>
2018-09-05 12:58:24 +0300

    Deleted file path and addResource method

--- src/main/scala/com/ua/Main.scala
+++ src/main/scala/com/ua/Main.scala
@@ -3,18 +3,15 @@ package com.ua
 
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
 
 object Main extends App {
 
   val myConf: Config = ConfigFactory.load()
-  val file = myConf.getString("hive.path")
   val dbName = myConf.getString("hive.database")
   val tableName = myConf.getString("hive.table")
   val columnName = myConf.getString("hive.column")
   val dateFormat = "yyyy-MM-dd"
   val hadoopConfig = new Configuration()
-  hadoopConfig.addResource(new Path(file))
 
   val myClient = new HiveMetastoreClient(hadoopConfig)
 
