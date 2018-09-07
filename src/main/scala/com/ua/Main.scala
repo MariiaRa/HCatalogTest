81069be03afeb797a7199219cfa9b2dcfe3551d9
Maria Rabyk <mariia.rabyk@gmail.com>
2018-09-07 17:33:27 +0300

    Added new methods to test with hive table (one partition)

--- src/main/scala/com/ua/Main.scala
+++ src/main/scala/com/ua/Main.scala
@@ -24,22 +24,27 @@ object Main extends App {
   )
 
   val maxBatchId = myClient.getMaxBatchId(tableName)
-  println(s"max batchId: $maxBatchId")
+  println(s"Max batchId: $maxBatchId")
+  println(s"Max batchId in table with one partition: ${myClient.getMaxBatchId("data_one")}")
 
   val maxBatchIdwithFilter = myClient.getMaxBatchId(tableName, "dateid", "2018-08-31-17-57")
-  println(s"max batchId with filter: $maxBatchIdwithFilter")
+  println(s"Max batchId with filter: $maxBatchIdwithFilter")
 
-  println(s"max batchId in partition: ${myClient.getMaxPartitionId(tableName, "dateid")}")
+  println(s"Max batchId in partition: ${myClient.getMaxPartitionId(tableName, "dateid")}")
 
-  println(s"max batchId in partition with filter: ${myClient.getMaxPartitionId(tableName, "dateid", "dateid", "2018-08-31-17-57")}")
+  println(s"Max batchId in partition in table with one partition: ${myClient.getMaxPartitionId("data_one", "rddid")}")
 
-  println(s"range of max batchIds with filter: ${myClient.getMaxPartitionIdRange(1535727470003L, tableName, "dateid", Some("dateid", "2018-08-31-17-57"))}")
+  println(s"Max batchId in partition with filter: ${myClient.getMaxPartitionId(tableName, "dateid", "dateid", "2018-08-31-17-57")}")
+
+  println(s"Range of max batchIds with filter: ${myClient.getMaxPartitionIdRange(1535727470003L, tableName, "dateid", Some("dateid", "2018-08-31-17-57"))}")
 
   val maxDate = myClient.getMaxDate(tableName, "dateid")
   println(s"Max date id: $maxDate")
 
   val maxBatchIdRange = myClient.getBatchIdRange(1535727470003L, tableName)
   println(s"Batchid range: $maxBatchIdRange")
+
+  println(s"Batchid range in table with one partition: ${myClient.getBatchIdRange(1536324390002L, "data_one")}")
 
   val dateRange = myClient.getDateRange(tableName, 1535727470003L)
   println(s"Date map: $dateRange")
