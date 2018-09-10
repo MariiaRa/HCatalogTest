f1adf86908821b2c6954339e0411fb87f36748a1
Maria Rabyk <mariia.rabyk@gmail.com>
2018-09-10 13:52:02 +0300

    Deleted HiveMetaStoreClient code

--- src/main/scala/com/ua/Main.scala
+++ src/main/scala/com/ua/Main.scala
@@ -24,11 +24,9 @@ object Main extends App {
   val maxBatchIdwithFilter = myClient.getMaxBatchId(tableName, "dateid", "2018-08-31-17-57")
   println(s"Max batchId with filter: $maxBatchIdwithFilter")
 
-  println(s"Max batchId in partition: ${myClient.getMaxPartitionId(tableName, "dateid")}")
+  println(s"Max batchId in partition: ${myClient.getMaxPartitionId(tableName, "rddid")}")
 
-  println(s"Max batchId in partition in table with one partition: ${myClient.getMaxPartitionId("data_one", "rddid")}")
-
-  println(s"Max batchId in partition with filter: ${myClient.getMaxPartitionId(tableName, "dateid", "dateid", "2018-08-31-17-57")}")
+  println(s"Max batchId in partition with filter: ${myClient.getMaxPartitionId(tableName, "rddid", "dateid", "2018-08-31-17-57")}")
 
   println(s"Range of max batchIds with filter: ${myClient.getMaxPartitionIdRange(1535727470003L, tableName, "dateid", Some("dateid", "2018-08-31-17-57"))}")
 
@@ -45,16 +43,5 @@ object Main extends App {
 
   val dateRangeTwo = myClient.getDateRange(tableName, 1535727470003L, "dateid", "2018-08-31-17-57")
   println(s"Date map with filter: $dateRangeTwo")
-
-  /* val configsHive = new HiveConf
-   configsHive.set("javax.jdo.option.ConnectionURL", "jdbc:postgresql://hive-metastore-postgresql/metastore")
-   configsHive.set("javax.jdo.option.ConnectionDriverName", "org.postgresql.Driver")
-   configsHive.set("javax.jdo.option.ConnectionPassword", "hive")
-   configsHive.set("javax.jdo.option.ConnectionUserName", "hive")
-   configsHive.set("hive.metastore.uris", "thrift://hive-metastore:9083")
-   val metastoreClient = new HiveMetaStoreClient(configsHive)
-
-   println("All tables from db: " + metastoreClient.getAllTables("default"))
-   println("All partition values: " + metastoreClient.listPartitions("default", tableName, -1).asScala.toList.map(_.getValues))*/
 
 }
